d0eca9064eb3cfc6705d51f090f92e0e9c8cd6d3
fiona <1125575769@qq.com>
2020-07-24 16:40:54 +0800

    轮播图更新

--- 分享/设计模式/code/slideShow.js
+++ 分享/设计模式/code/slideShow.js
@@ -1,36 +1,5 @@
-//轮播图
 
-var options = {
-    id:'imgContent',
-    selected: 'item2',
-    images: [{
-        id: 1,
-        name: 'item1',
-        src: './static/img/1.jpg'
-    }, {
-        id: 2,
-        name: 'item2',
-        src: './static/img/2.jpg'
-    }, {
-        id: 3,
-        name: 'item3',
-        src: './static/img/3.jpg'
-    }, {
-        id: 4,
-        name: 'item4',
-        src: './static/img/4.jpg'
-    }, {
-        id: 5,
-        name: 'item5',
-        src: './static/img/5.jpg'
-    }, {
-        id: 6,
-        name: 'item6',
-        src: './static/img/6.jpg'
-    }]
-}
-var pic = new Picture(options);
-class Picture {
+class Slide {
     constructor(options) {
         this.html = '';
         this.options = options;
@@ -42,8 +11,9 @@ class Picture {
         //2.防止用户漏传入
         var final = {};
         var defaultOptions = {
+            id:'',
             images: [],
-            renderId: document,          
+            renderId: document,
         };
         //适配器模式
         for (let item in defaultOptions) {
@@ -59,86 +29,53 @@ class Picture {
         return final;
     }
     initDOM(finalOptions) {
-        //需要样式
-        var styleArr = [{
-            //包裹图片div样式
-            float: 'left',
-            width: finalOptions.size[0] + 'px',
-            height: finalOptions.size[1] + 'px',
-            position: 'relative'
-
-        }, {
-            //下标顺序的样式
-            position: 'absolute',
-            bottom: '0px',
-            right: '0px',
-            background: 'black',
-            color: 'white',
-            padding: '10px',
-            width: '10px',
-            height: '10px'
-
-        }, {
-            //图片样式
-            width: '100%',
-            height: '100%'
-        }];
-        var wraper = document.createElement('div');
-        //排序图片
-        //用策略模式
-        var commanderHandle = {
-            normal: function(arr) {
-                return arr;
-            },
-            reverse: function(arr) {
-                return arr.reverse();
-            }
-        }
-        var data = commanderHandle[finalOptions.sort](finalOptions.data);
-        data.forEach((url, index) => {
-            var div = document.createElement('div');
-            var img = document.createElement('img');
-            var span = document.createElement('span');
-            var styleObj = null;
-            var handleDom = null;
-            styleArr.forEach((style, index) => {
-                //享元模式
-                switch (index) {
-                    case 0:
-                        handleDom = div;
-                        break;
-                    case 1:
-                        handleDom = span;
-                        break;
-                    case 2:
-                        handleDom = img;
-                        break;
-                }
-                for (var styleName in style) {
-                    handleDom.style[styleName] = style[styleName];
-                }
+            var data = finalOptions.images;
+            data.forEach((image, index) => {
+                var item = new SlideItem({ image: image });
+                this.items.push(item);
+                var itemEle = item.getDOM();
+                finalOptions.id.appendChild(itemEle);
             });
-            img.setAttribute('src', url);
-            div.appendChild(img);
-            span.innerHTML = index + 1;
-            div.appendChild(span);
-            wraper.appendChild(div);
-        });
-        this.html = wraper;
-    }
-    renderDOM(element) {
-        element.appendChild(this.html);
+        }
+        //每次轮播的时候把name的值进行调整，然后发送到调用该组件的容器
+    play(options) {
+        this.timer = setInterval(() => {
+            const index = this.ids.indexOf(this.current);
+            let newIndex = index + 1;
+            if (newIndex === this.ids.length) {
+                newIndex = 0;
+            }
+            var itemObj = this.items[newIndex];           
+            itemObj.updateSwiperItem();
+        }, 3000);
     }
     render() {
-        var order = this.initData();
-        this.initDOM(order);
-        this.renderDOM(order.id);
+        var options = this.initData();
+        this.ids = [];
+        this.items = [];
+        options.images.forEach((option, index) => {
+            this.ids.push(option.id);
+        });
+        this.current = this.ids[0];
+        this.initDOM(options);
+        this.play(options);
     }
 }
 
-class Slide {
-
-}
 class SlideItem {
-
+    constructor(options) {
+        this.options = options;
+    }
+    updateSwiperItem() {
+        this.ele.classList.add('animation');
+    }
+    getDOM() {
+        var image = this.options.image;
+        var div = document.createElement('div');
+        var img = document.createElement('img');
+        img.setAttribute('src', image.src);
+        div.appendChild(img);
+        this.ele = div;
+        return div;
+    }
 }
